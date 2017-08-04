e390ec8018d08009343a8aac17703209308c5bf3
dantin <chengjie.ding@gmail.com>
2017-08-04 21:42:50 +0800

    non recursive solution

--- merge-two-binary-trees/solution.go
+++ merge-two-binary-trees/solution.go
@@ -1,8 +1,39 @@
 package p617
 
-import "github.com/dantin/leetcode-go/tree"
+import (
+	"github.com/dantin/leetcode-go/stack"
+	"github.com/dantin/leetcode-go/tree"
+)
 
 func MergeTrees(t1 *tree.TreeNode, t2 *tree.TreeNode) *tree.TreeNode {
+	if t1 == nil {
+		return t2
+	}
+
+	ss := stack.New()
+	ss.Push([]*tree.TreeNode{t1, t2})
+	for !ss.IsEmpty() {
+		t := ss.Pop().([]*tree.TreeNode)
+		if t[0] == nil || t[1] == nil {
+			continue
+		}
+		t[0].Val += t[1].Val
+		if t[0].Left == nil {
+			t[0].Left = t[1].Left
+		} else {
+			ss.Push([]*tree.TreeNode{t[0].Left, t[1].Left})
+		}
+		if t[0].Right == nil {
+			t[0].Right = t[1].Right
+		} else {
+			ss.Push([]*tree.TreeNode{t[0].Right, t[1].Right})
+		}
+	}
+	return t1
+}
+
+// recursive version
+func MergeTrees1(t1 *tree.TreeNode, t2 *tree.TreeNode) *tree.TreeNode {
 	if t1 == nil {
 		return t2
 	}
